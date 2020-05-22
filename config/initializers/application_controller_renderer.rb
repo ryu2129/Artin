acf46ab12978f5a2e18b2fc344b09b90ae449bfe
ryu2129 <ryu212920@gmail.com>
2020-05-22 10:20:15 +0000

    add user controller

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '6c118fb34e21b2dcae07e904e9841b233fc894f1ba7c03fb18b94ff14b6bf9eced672be6722cd533bf894eb5c238e6869f4d2cb5a5e164570e42ac90d14bb056'
